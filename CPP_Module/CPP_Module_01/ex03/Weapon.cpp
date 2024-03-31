30636bd69324b234b75a2d27d17864900219f2d4
Jae hyun Kang <jae-kang@c4r2s8.42seoul.kr>
2024-03-31 13:36:59 +0900

    .

--- CPP_Module/CPP_Module_01/ex03/Weapon.cpp
+++ CPP_Module/CPP_Module_01/ex03/Weapon.cpp
@@ -4,7 +4,7 @@ Weapon::Weapon(const std::string _type) : type(_type)
 {
 }
 
-const std::string	Weapon::getType(void)
+const std::string	&Weapon::getType(void)
 {
 	return type;
 }
--- CPP_Module/CPP_Module_01/ex03/Weapon.hpp
+++ CPP_Module/CPP_Module_01/ex03/Weapon.hpp
@@ -9,7 +9,7 @@ class	Weapon
 		std::string	type;
 	public:
 		Weapon(const std::string _type);
-		const std::string	getType(void);
+		const std::string	&getType(void);
 		void				setType(const std::string _type);
 };
 
